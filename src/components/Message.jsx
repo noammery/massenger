c5dc2505eb38b9299e22fab69aef1000c4046c4b
noammery <noammery55@gmail.com>
2022-10-26 20:07:54 +0300

    hitory 2

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -8,7 +8,7 @@ import Chat from "./Chat";
 
 const Message = () => {
   // https://nmmessanger-api.herokuapp.com
-  const socket = io.connect(`http://localhost:3001`, {
+  const socket = io.connect(`https://nmmessanger-api.herokuapp.com`, {
     withCredentials: true,
     transports: ["websocket"],
   });
